de84515a4d1ee187f4305a3f0b7ddae4be9d4f46
Fabio Donadelli <fabio.donadelli@kabum.com.br>
2021-03-24 21:42:39 +0000

    test: delete and update tests for category

--- tests/Feature/Models/CategoryTest.php
+++ tests/Feature/Models/CategoryTest.php
@@ -40,6 +40,7 @@ class CategoryTest extends TestCase
         ]);
         $category->refresh();
 
+        $this->assertEquals(36, strlen($category->id));
         $this->assertEquals('test1', $category->name);
         $this->assertNull($category->description);
         $this->assertTrue((bool)$category->is_active);
@@ -67,7 +68,7 @@ class CategoryTest extends TestCase
         $category = factory(Category::class)->create([
             'description' => 'test_description',
             'is_active' => false
-        ])->first();
+        ]);
 
         $data =[
             'name' => 'update_name',
@@ -79,9 +80,14 @@ class CategoryTest extends TestCase
         foreach ($data as $key => $value) {
             $this->assertEquals($value, $category->{$key});
         };
+    }
 
+    public function testeDelete(){
+        $category = factory(Category::class)->create();
+        $category->delete();
+        $this->assertNull(Category::find($category->id));
 
-
-
+        $category->restore();
+        $this->assertNotNull(Category::find($category->id));
     }
 }
